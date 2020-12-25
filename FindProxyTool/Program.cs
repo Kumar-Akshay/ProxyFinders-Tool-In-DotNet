780099418182eb956f268d661795a00da03e36fd
kumar-Akshay <akshaymalhi012@gmail.com>
2020-12-25 19:34:23 +0500

    Updated Commit

---
+++ FindProxyTool/Program.cs
@@ -0,0 +1,158 @@
+﻿using FindProxyTool.AuthGG;
+using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace FindProxyTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Appname = "MultaWare";
+            var AID = "316212";
+            var ProgramSecret = "Ug1rd9JTTZj4EuxkWD5QP9nYUc4bprhj7KN";
+            var Version = "1.0";
+
+            //This connects your file to the Auth.GG API, and sends back your application settings and such
+            OnProgramStart.Initialize(Appname, AID, ProgramSecret, Version);
+            var option = MainMenu();
+            if (option == "1")
+            {
+                if (!ApplicationSettings.Register)
+                {
+                    //Register is disabled in application settings, will show a messagebox that it is not enabled
+                    System.Windows.MessageBox.Show("Register is not enabled, please try again later!", OnProgramStart.Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    Process.GetCurrentProcess().Kill(); //closes the application
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine();
+                    Console.WriteLine("Username:");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("Password:");
+                    string password = Console.ReadLine();
+                    Console.WriteLine("Email:");
+                    string email = Console.ReadLine();
+                    Console.WriteLine("License:");
+                    string license = Console.ReadLine();
+                    if (API.Register(username, password, email, license))
+                    {
+                        System.Windows.MessageBox.Show("You have successfully registered!", OnProgramStart.Name, MessageBoxButton.OK, MessageBoxImage.Information);
+                        // Do code of what you want after successful register here!
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        System.Windows.MessageBox.Show("You have not registered!", OnProgramStart.Name, MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+            }
+            else if (option == "2")
+            {
+                if (!ApplicationSettings.Login)
+                {
+                    //Register is disabled in application settings, will show a messagebox that it is not enabled
+                    System.Windows.MessageBox.Show("Login is not enabled, please try again later!", OnProgramStart.Name, MessageBoxButton.OK, MessageBoxImage.Error);
+                    Process.GetCurrentProcess().Kill(); //closes the application
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine();
+                    Console.WriteLine("Username:");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("Password:");
+                    string password = Console.ReadLine();
+                    if (API.Login(username, password))
+                    {
+                        System.Windows.MessageBox.Show("You have successfully logged in!", OnProgramStart.Name, MessageBoxButton.OK, MessageBoxImage.Information);
+                        Console.Clear();
+                        // Success login stuff goes here
+                        Console.ForegroundColor = ConsoleColor.White;
+                        API.Log(username, "Logged in!"); //Logs this action to your web-panel, you can do this anywhere and for anything!
+                        Console.WriteLine("***************************************************");
+                        Console.WriteLine("All user information:");
+                        Console.WriteLine("***************************************************");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"User ID -> {User.ID}");
+                        Console.WriteLine($"Username -> {User.Username}");
+                        Console.WriteLine($"Password -> {User.Password}");
+                        Console.WriteLine($"Email -> {User.Email}");
+                        Console.WriteLine($"HWID -> {User.HWID}");
+                        Console.WriteLine($"User Variable -> {User.UserVariable}");
+                        Console.WriteLine($"User Rank -> {User.Rank}");
+                        Console.WriteLine($"User IP -> {User.IP}");
+                        Console.WriteLine($"Expiry -> {User.Expiry}");
+                        Console.WriteLine($"Last Login -> {User.LastLogin}");
+                        Console.WriteLine($"Register Date -> {User.RegisterDate}");
+                        Console.WriteLine($"Variable -> {App.GrabVariable("Ug1rd9JTTZj4EuxkWD5QP9nYUc4bprhj7KN")}"); // Replace put variable secret here with the secret of the variable in your panel - https://i.imgur.com/v3q2a6e.png
+                        Console.WriteLine("***************************************************");
+                        Console.WriteLine("***************************************************");
+                        Console.WriteLine("\n\n");
+                        var proxy = new ProxyFinder();
+                        proxy.FindProxyTool();
+                        Console.WriteLine("***************************************************");
+                        Console.WriteLine("***************************************************");
+                        Console.ReadKey();
+                    }
+                }
+            }
+            else if (option == "3")
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("Username:");
+                string username = Console.ReadLine();
+                Console.WriteLine("Password:");
+                string password = Console.ReadLine();
+                Console.WriteLine("License Token:");
+                string token = Console.ReadLine();
+                if (API.ExtendSubscription(username, password, token))
+                {
+                    System.Windows.MessageBox.Show("You have successfully extended your subscription!", OnProgramStart.Name, MessageBoxButton.OK, MessageBoxImage.Information);
+                    // Do code of what you want after successful extend here!
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        public static string MainMenu()
+        {
+            PrintLogo();
+            Console.WriteLine("[1] Register");
+            Console.WriteLine("[2] Login");
+            Console.WriteLine("[3] Extend Subscription");
+            while (true)
+            {
+                Console.Write("\nEnter your choice: ");
+                string option = Console.ReadLine();
+                if (option == "1" || option == "2" || option == "3")
+                {
+                    return option;
+                }
+                Console.WriteLine("\nInvaild Choice, Try again");
+            }
+        }
+    
+        public static void PrintLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine();
+            string Logo = @"
+  _____   _____    ____ __   ____     __  ______  _____  _   _  _____   ______  _____  
+ |  __ \ |  __ \  / __ \\ \ / /\ \   / / |  ____||_   _|| \ | ||  __ \ |  ____||  __ \ 
+ | |__) || |__) || |  | |\ V /  \ \_/ /  | |__     | |  |  \| || |  | || |__   | |__) |
+ |  ___/ |  _  / | |  | | > <    \   /   |  __|    | |  | . ` || |  | ||  __|  |  _  / 
+ | |     | | \ \ | |__| |/ . \    | |    | |      _| |_ | |\  || |__| || |____ | | \ \ 
+ |_|     |_|  \_\ \____//_/ \_\   |_|    |_|     |_____||_| \_||_____/ |______||_|  \_\
+                                                                                       ";
+            Console.WriteLine(Logo);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
---
+++ FindProxyTool/ProxyFinder.cs
@@ -0,0 +1,163 @@
+﻿using HtmlAgilityPack;
+using Leaf.xNet;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FindProxyTool
+{
+    public class ProxyFinder
+    {
+        // Loading the source links
+        List<string> ProxySource = new List<string>()
+            {
+                "http://free-proxy-list.net/anonymous-proxy.html",
+                "http://proxydb.net/",
+                "https://www.us-proxy.org/",
+                //"http://www.proxylists.net/http_highanon.txt",
+                //"https://www.duplichecker.com/free-proxy-list.php",
+                //"https://free-proxy-list.net",
+                //"https://www.sslproxies.org",
+                //"http://www.proxylists.net/http_highanon.txt",
+                //"http://rootjazz.com/proxies/proxies.txt",
+                //"http://www.proxyserverlist24.top/2020/05/26-05-20-free-proxy-server-list-2864.html",
+                //"http://proxyserverlist-24.blogspot.com/feeds/posts/default",
+                //"https://smallseotools.com/free-proxy-list/"
+            };
+
+        public Regex REGEX = new Regex(@"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\:[0-9]{1,5}\b");
+        List<string> scraped = new List<string>();
+        List<string> checkedProxy = new List<string>();
+        string CurrentlyScrapingPage;
+
+        public ProxyFinder() { }
+
+
+        public void FindProxyTool()
+        {
+            int goodProxy = 0;
+            Console.WriteLine("\n\n Please wait for Scraping the Proxies");
+            int Totalproxy = ScrapeProxy();
+            var proxies = File.ReadLines("../../../ScrapedProxies.txt");
+
+            Console.WriteLine("\nEnter the Proxy Timeout in milliseconds (like 1000, 2000) : ");
+            Console.Write("\nTimeout : ");
+            var proxyTimeout = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter the Number of Threads : ");
+            Console.Write("\nThreads : ");
+            var Threads = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nSelect Type of Proxy: ");
+            Console.WriteLine("1. HTTP ProxyType");
+            Console.WriteLine("2. SOCK4 ProxyType");
+            Console.WriteLine("3. SOCK5 ProxyType\n");
+            Console.WriteLine("Select your choice: ");
+            var choices = Console.ReadLine();
+            Console.WriteLine("\n Please wait we are checking the proxies, save into the file \n");
+            int choice = Convert.ToInt32(choices);
+            ProxyType proxyType;
+            switch (choice)
+            {
+                case 1:
+                    proxyType = ProxyType.HTTP;
+                    goodProxy = ProxyChecker(proxyType, proxies, proxyTimeout, Threads);
+                    Console.WriteLine("\n Total Number of Proxies: " + Totalproxy);
+                    Console.WriteLine("\n Total Number of Good Proxy: " + goodProxy);
+                    break;
+                case 2:
+                    proxyType = ProxyType.Socks4;
+                    goodProxy = ProxyChecker(proxyType, proxies, proxyTimeout, Threads);
+                    Console.WriteLine("\n Total Number of Proxies: " + Totalproxy);
+                    Console.WriteLine("\n Total Number of Good Proxy: " + goodProxy);
+                    break;
+                case 3:
+                    proxyType = ProxyType.Socks5;
+                    goodProxy = ProxyChecker(proxyType, proxies, proxyTimeout, Threads);
+                    Console.WriteLine("\n Total Number of Proxies: " + Totalproxy);
+                    Console.WriteLine("\n Total Number of Good Proxy: " + goodProxy);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public int ScrapeProxy()
+        {
+            File.Delete("../../../ScrapedProxies.txt");
+            System.Net.WebClient _WC = new System.Net.WebClient();
+            try
+            {
+                foreach (string PageToScrape in ProxySource)
+                {
+                    Console.WriteLine("[*] Scraping the " + PageToScrape);
+                    string PageSource = _WC.DownloadString(PageToScrape);
+                    CurrentlyScrapingPage = PageToScrape;
+
+                    MatchCollection _MC = REGEX.Matches(PageSource);
+                    foreach (Match Match in _MC)
+                    {
+                        scraped.Add(Match.ToString());
+                        //Console.WriteLine(Match.ToString());
+                        File.AppendAllText("../../../ScrapedProxies.txt", Match.ToString() + Environment.NewLine);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("[*]" + "Bad Request  " + CurrentlyScrapingPage);
+            }
+            SaveFile(scraped, "../../../ScrapedProxies.txt");
+            Console.WriteLine("Total Number of Proxies Scraped Found : " + scraped.Count);
+            return scraped.Count;
+        }
+
+
+        public int ProxyChecker(ProxyType proxyType, IEnumerable<String> proxies, int proxyTimeout, int threads)
+        {
+            int correctProxy = 0;
+            var requestTimeout = 5 * 1000;
+            File.Delete("../../../GoodProxy.txt");
+            Parallel.ForEach(proxies, new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = threads
+            }, proxy =>
+            {
+                try
+                {
+                    using (var request = new HttpRequest())
+                    {
+                        request.ConnectTimeout = requestTimeout;
+                        request.Proxy = ProxyClient.Parse(proxyType, proxy);
+                        request.Proxy.ConnectTimeout = proxyTimeout;
+                        request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36";
+                        request.Get("http://google.com");
+                        Console.WriteLine("[Good Proxy] " + proxy);
+                        checkedProxy.Add(proxy);
+                        correctProxy = correctProxy + 1;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("[Bad Proxy] " + proxy);
+                }
+            });
+            SaveFile(checkedProxy, "../../../GoodProxy.txt");
+            return correctProxy;
+        }
+
+        public void SaveFile(List<string> ProxyList, string path)
+        {
+            foreach (var proxy in ProxyList)
+            {
+                File.AppendAllText(path, proxy + Environment.NewLine);
+            }
+        }
+    }
+}
